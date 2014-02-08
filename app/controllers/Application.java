e6611b6b5cc156f54709589bcd511bab462559a9
Neha Agrawal <neha.agrawal3093@gmail.com>
2014-02-08 20:58:49 +0000

    I am very clever

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,9 +2,9 @@ package controllers;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 import play.*;
 import play.mvc.*;
-
 import views.html.*;
 
 import java.io.BufferedReader;
@@ -16,6 +16,7 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+
 import main.*;
 
 public class Application extends Controller {
@@ -27,6 +28,15 @@ public class Application extends Controller {
     public static Result index() {
         Advert advert = AutoTraderAPI.getRandomAdvert();
         List<String> images = AutoTraderAPI.getAdvertImageLinks(advert.getId());
+        Mongo db;
+		try {
+			db = new Mongo();
+			db.insertAdvert(advert.getId(), advert.getPrice(), images);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
 //        String image = "";
 //        for(String imageLink: images) {
 //            image = image + imageLink + "\n";
--- app/main/Mongo.java
+++ app/main/Mongo.java
@@ -1,4 +1,4 @@
-package controllers;
+package main;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoException;
 import com.mongodb.WriteConcern;
@@ -10,16 +10,17 @@ import com.mongodb.DBCursor;
 import com.mongodb.ServerAddress;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class Mongo 
 {
 	private DBCollection coll;
-	public Mongo()
+	public Mongo()throws Exception
 	{
 		MongoClient mongoClient = new MongoClient( "widmore.mongohq.com" , 10000 );
-		DB db = mongoClient.getDB( "mydb" );
+		DB db = mongoClient.getDB( "StudentHack" );
 		
-		boolean auth = db.authenticate("studenthack", "reallyStrongPassword");
+		boolean auth = db.authenticate("studenthack", "jkgfhjgfdojnglegjdso".toCharArray());
 		
 		if(auth)		
 			coll = db.getCollection("advert");
@@ -27,31 +28,31 @@ public class Mongo
 			throw new Exception();
 	}
 	
-	public void insertMongo(String id, int price, List<String> links)
+	public void insertAdvert(String id, int price, List<String> links)
 	{
 		BasicDBObject in = new BasicDBObject("id", id).
 							append("price", price).
 							append("links", links);
-		coll.insert(doc);
+		coll.insert(in);
 	}
 	
+	/*
 	public int findMongo(String search)
 	{
 		BasicDBObject query = new BasicDBObject("id", search);
 		
-		DBCursor cursor = coll.find(query).fetch();
-		int result;
+		DBCursor cursor = coll.find(query);
+		DBO result;
 		
 		try{
 			while(cursor.hasNext())
-				cursor.next();
-			result = cursor.get().get("price");
+				result = cursor.next().get("price");
 		}
 		finally{
 			cursor.close();
 		}
 		return result;		
-	}// End findMongo
+	}// End findMongo */
 	
 	
 }
